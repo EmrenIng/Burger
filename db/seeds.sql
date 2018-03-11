71b5a60fb43fdd4058da1a86200f78cdfad86489
EmrenIng <Ingvoldstad.Maya@gmail.com>
2018-03-11 17:12:53 -0500

    Revert "app"
    This reverts commit d1c66001f3c152ebbb85bf47b4b88dd4e1a14999.

--- db/seeds.sql
+++
@@ -1,5 +0,0 @@
-USE burger_db;
-
-INSERT INTO burgers (burger_name, devoured) VALUES ('Cheeseburger', FALSE);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Hamburger', FALSE);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Plain Burger', FALSE);
